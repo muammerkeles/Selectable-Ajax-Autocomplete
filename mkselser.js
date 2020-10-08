4380ef0bb9dc9f6ea1f3dc5127cc1d469c6d86c6
Muammer K <contact@muammerkeles.com>
2020-10-08 23:03:12 +0300

    bıg fixes
    jquery ui dependency is removed

--- mkselser.js
+++ mkselser.js
@@ -1,7 +1,7 @@
-﻿/*!
+/*!
  * Ajax Autocomplete Selectbox, by Muammer Keleş.
+ * v2.0.1
  */
-
 ; (function ($) {
     var defaults = {
         multipleSelect: false,
@@ -9,7 +9,7 @@
         minChar: 2,
         delay: 300,
         onchange: function () {
-            
+
         },
         placeholder: "Select",
         searchHolder: "Search something",
@@ -23,7 +23,7 @@
             responsefields: null,
             responseKey: null,
             displayfields: [],
-            valuekey:null,
+            valuekey: null,
             attributes: []
         },
         defaultdata: null //  {display:"venualdja ldkaj dlak dla",inputAttributes: { "venueid": "5" }},
@@ -34,7 +34,7 @@
         var $options = $.extend({}, defaults, _opt);
         var $this = this;
         var _value = "";
-        var panel = CreateSelSerObject($el);
+        var panel = new CreateSelSerObject($el);
         var focusIndex = -1;
         var cachedata = {
             data: {},
@@ -77,32 +77,35 @@
             if ($options.ajaxParams.responsefields == null) {
                 console.log("responsefields for Ajax-Url wasn't defined!"); return false;
             }
-            var keyCode = $.ui.keyCode;
+            //var keyCode = $.ui.keyCode;
             panel.input.on("keyup input", function (event) {
+                console.log("event.keyCode", event.keyCode);
                 switch (event.keyCode) {
-                    case keyCode.PAGE_UP:
+                    case 33:
                         _keyEvent("previousPage", event);
                         break;
-                    case keyCode.PAGE_DOWN:
+                    case 34:
                         _keyEvent("nextPage", event);
                         break;
-                    case keyCode.UP:
+                    case 38:
                         _keyEvent("previous", event);
                         break;
-                    case keyCode.DOWN:
+                    case 40:
+                        console.log("40a basıldı");
+
                         _keyEvent("next", event);
                         break;
-                    case keyCode.ENTER:
+                    case 13:
 
                         if (focusIndex > -1) {
                             event.preventDefault();
                             selectItem(event);
                         }
                         break;
-                    case keyCode.TAB:
+                    case 9:
 
                         break;
-                    case keyCode.ESCAPE:
+                    case 27:
                         if (panel.input.is(":visible")) {
                             close_it();
                             event.preventDefault();
@@ -118,7 +121,7 @@
 
         if ($options.defaultdata != null) {
             var _dfdata = $options.defaultdata;
-            var _df_display= $options.defaultdata.display;
+            var _df_display = $options.defaultdata.display;
             var _df_inp_attr = $options.defaultdata.inputAttributes;
             panel.label.html(_df_display);
             $.each(_df_inp_attr, function (x, y) {
@@ -164,15 +167,15 @@
 
 
         var search = function (value, event) {
-            value = value != null ? value : $this._value; 
+            value = value != null ? value : $this._value;
             if (value.length < $options.minChar) {
                 panel.input.removeClass("inp-loading");
                 console.log("min char is less than val", value);
                 return;//this.close(event);
             }
-            return _search(value); 
+            return _search(value);
         }
-        var _search = function (value) { 
+        var _search = function (value) {
             var _staticparams = "";
             if ($options.ajaxParams.staticParams != null) {
                 $.each($options.ajaxParams.staticParams, function (si, sv) {
@@ -182,7 +185,7 @@
 
             var $_uri = $options.ajaxParams.url + "?" + _staticparams + $options.ajaxParams.paramName + "=" + value;
             console.log("requested", $_uri);
-            panel.input.addClass("inp-loading"); 
+            panel.input.addClass("inp-loading");
             $.ajax({
                 url: $_uri, type: "GET",
                 dataType: $options.ajaxParams.dataType,
@@ -198,7 +201,7 @@
 
                 },
                 success: function (_data) {
-                     
+
                     var dt = _data;
                     if ($options.ajaxParams.responseKey != null && $options.ajaxParams.responseKey != "") {
                         dt = _data[$options.ajaxParams.responseKey];
@@ -207,7 +210,7 @@
                         cachedata.set($_uri, dt);
                     }
                     console.log("CACHE DID NOT USE!");
-                    
+
                     ShowResult(dt);
 
                 }
@@ -217,14 +220,14 @@
 
         function ShowResult(dt) {
             focusIndex = -1;
-            panel.container.find(".ul-res").html(""); 
+            panel.container.find(".ul-res").html("");
             if (dt.length <= 0) {
                 var obj = ["No result"];
                 panel.container.find(".ul-res").append("<li class='litems' disabled> noresult</li>");
-            } else { 
+            } else {
 
                 $.each(dt, function (x, item) {
-                    var displayfs="",attrs="";
+                    var displayfs = "", attrs = "";
                     $.each($options.ajaxParams.displayfields, function (x, y) {
                         displayfs += item[y] + " ";
                     });
@@ -260,12 +263,12 @@
         //panel.container.on("focusout", function (focusEvent) {
         //    /console.log("secondTarget ", focusEvent.relatedTarget)
         //});
-        panel.container.on("click", ".litems", function () { 
+        panel.container.on("click", ".litems", function () {
             if ($(this).attr("disabled")) return;
             var clickitem = $(this);
             panel.label.html($(this).html());
             selecteElement = $(this);
-            $.each($options.ajaxParams.inputAttributes, function (x, y) { 
+            $.each($options.ajaxParams.inputAttributes, function (x, y) {
                 applySelect(y, clickitem.attr("data-" + y));
             });
             focusIndex = $(this).index();
@@ -283,34 +286,34 @@
         }
         function _isEventTargetInWidget(event) {
             //var par = $(event.target).parent();
-            
+
             var menuElement = panel.container[0];
             var selectElement = panel.span[0];
 
             return event.target === selectElement ||
                 event.target === menuElement ||
-                $.contains(selectElement, event.target) || 
+                $.contains(selectElement, event.target) ||
                 $.contains(menuElement, event.target);
         };
-      
+
         $(document).on("mousedown", function (e) {
             if (!_isEventTargetInWidget(e)) {
                 close_it();
             }
         });
         function _keyEvent(keyEvent, event) {
-            if (panel.container.is(":visible")) { 
+            if (panel.container.is(":visible")) {
                 _move(keyEvent, event);
                 // Prevents moving cursor to beginning/end of the text field in some browsers
                 event.preventDefault();
-            } 
+            }
         };
         function _move(direction, event) {
             switch (direction) {
                 case "previous":
                     focusIndex--;
                     if (focusIndex < 0) {
-                        focusIndex = panel.container.find("li").length-1;
+                        focusIndex = panel.container.find("li").length - 1;
                         panelMenuAnimate(panel.ulmenu.height());
                     }
                     focusItem(focusIndex);
@@ -320,7 +323,7 @@
                     if (focusIndex > panel.container.find("li").length - 1) {
                         panelMenuAnimate(0);
                         focusIndex = 0;
-                    }  
+                    }
                     focusItem(focusIndex);
                     break;
 
@@ -343,23 +346,23 @@
             }
         };
         function panelMenuAnimate(st, speed) {
-                panel.ulmenu.animate({
-                    scrollTop: st
-                }, speed == undefined ? 200 : speed);
+            panel.ulmenu.animate({
+                scrollTop: st
+            }, speed == undefined ? 200 : speed);
         }
         function focusItem(focusIndex) {
             var item = panel.container.find("li").eq(focusIndex);
             panel.container.find("li.focus").removeClass("focus");
             item.addClass("focus");
-            
+
             var itemtop = item.position().top;
             var itemheight = item.outerHeight();
             var ulpositiontop = panel.ulmenu.position().top;
             var ulheight = panel.ulmenu.outerHeight();
-            if ((ulheight + ulpositiontop < itemtop + itemheight) ||(0 >itemtop - itemheight)) {
+            if ((ulheight + ulpositiontop < itemtop + itemheight) || (0 > itemtop - itemheight)) {
                 panelMenuAnimate(itemtop);
             }
-             
+
 
         }
         function selectItem() {
@@ -385,10 +388,11 @@
         });
     };
 
+    
 })(jQuery);
 
 var CreateSelSerObject = function (selv) {
-    
+
     var _p = selv;
 
     var _span = $('<span/>');
@@ -435,19 +439,21 @@ var CreateSelSerObject = function (selv) {
     _t.find(".res-container")
     _res_container.append(_res_below.append(_res_input_cover.append(_res_input_box), _res_wrap.append(_ulres)));
 
-    var _stop = _span.position().top  ; /// _span.height();
-    
+    var _stop = _span.position().top; /// _span.height();
+
     _res_container.css("width", _span.width());
     _res_container.css("left", 0);
-    _res_container.css("top", (_stop + _span.height())-10);
-       
+    _res_container.css("top", (_stop + _span.height()) - 10);
+
     $("body").append(_res_container)
 
     _p.hide();
     this.container = _res_container;
     this.ulmenu = _ulres;
-    this.span= _span;
+    this.span = _span;
     this.label = _sel_render;
     this.input = _res_input_box;
     return this;
 }
+/// end of begin__init
+ 
